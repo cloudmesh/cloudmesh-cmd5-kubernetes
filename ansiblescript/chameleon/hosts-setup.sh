d7fe8c20ab8699abf42b50d41ac658abddcea064
Sagar Vora <vorasagar7@gmail.com>
2017-04-26 23:36:24 -0400

    new:usr: deleted host file

--- ansiblescript/chameleon/hosts-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cm cluster nodes cluster-001|awk '{print $2 }'> ip_list
-sed -i 's/$/ ansible_ssh_user=cc/' ip_list
-sed -i 's/$/ ansible_ssh_private_key="\/home\/sagar\/.ssh\/id_rsa.pem"/' ip_list
-echo [kubernetes]|cat > inventory
-head -n2 -q ip_list | tail -n1 >> inventory 
-rm ip_list
