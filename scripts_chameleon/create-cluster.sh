d1c510753ef2a9e8c0472b45fba68b32b78cda71
Sagar Vora <vorasagar7@gmail.com>
2017-04-28 18:34:54 -0400

    chg:usr: modified sec group

--- scripts_chameleon/create-cluster.sh
+++ scripts_chameleon/create-cluster.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cm cluster define --count 3 --secgroup=kubernetes-secgroup --flavor=m1.medium --image=CC-Ubuntu16.04-20161214
+cm cluster define --count 3 --secgroup=mesos-secgroup --flavor=m1.medium --image=CC-Ubuntu16.04-20161214
 cm cluster allocate
