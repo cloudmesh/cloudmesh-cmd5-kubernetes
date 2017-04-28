6ba5ed60f25f7fb79f7493c21a3ec58739391cd2
Sagar Vora <vorasagar7@gmail.com>
2017-04-28 00:16:11 -0400

    new:usr: added files related to kilo deployment

--- scripts_chameleon/deploy-kubernetes.sh
+++ scripts_chameleon/deploy-kubernetes.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #ANSIBLE_HOST_KEY_CHECKING=False
-~/cloudmesh.kubernetes/scripts/create-cluster.sh
-~/cloudmesh.kubernetes/scripts/hosts-setup.sh
+~/cloudmesh.kubernetes/scripts_chameleon/create-cluster.sh
+~/cloudmesh.kubernetes/scripts_chameleon/hosts-setup.sh
 ansible-playbook ~/cloudmesh.kubernetes/ansiblescript/chameleon/kubernetes.yml -i ~/cloudmesh.kubernetes/ansiblescript/chameleon/inventory.txt
 ansible-playbook ~/cloudmesh.kubernetes/ansiblescript/chameleon/master.yml -i ~/cloudmesh.kubernetes/ansiblescript/chameleon/inventory.txt
-sh ~/cloudmesh.kubernetes/scripts/join-slaves.sh
+sh ~/cloudmesh.kubernetes/scripts_chameleon/join-slaves.sh
---
+++ scripts_kilo/join-slaves.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ansible-playbook ~/cloudmesh.kubernetes/ansiblescript/kilo/slaves.yml -i ~/cloudmesh.kubernetes/ansiblescript/kilo/inventory.txt
