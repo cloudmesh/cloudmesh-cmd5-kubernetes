996dea925f7ae68eb147c77cc044e8bc71eff1ef
Sagar Vora <vorasagar7@gmail.com>
2017-06-06 02:36:28 -0400

    new:usr: adding kubernetes command as a part of CMD5 module

--- cloudmesh/kubernetes/command/kubernetes.py
+++ cloudmesh/kubernetes/command/kubernetes.py
@@ -1,103 +1,125 @@
 from __future__ import print_function
 from cloudmesh.shell.command import command
 from cloudmesh.shell.command import PluginCommand
+from cloudmesh.common.ConfigDict import ConfigDict
+from cloudmesh.common.StopWatch import StopWatch
 from cloudmesh.common.default import Default
 from cloudmesh.common.console import Console
 from pprint import pprint
 
+import os
+
 class KubernetesCommand(PluginCommand):
+	def make_hosts(self):
+		path = os.getcwd()
+		os.system('cm cluster inventory > ' + path + '/hosts.txt')
+		f = open(path + '/hosts.txt', 'r')
+		w = open(path + '/hosts', 'w')
+		w.write("[cluster]\n")
+		for i, line in enumerate(f):
+			if i <= 0:
+				continue
+			else:
+				w.write("node{} host=".format(i) + line)
+		f.close()
+		w.write("\n[nimbus]\n")
+		f = open(path + '/hosts.txt', 'r')
+		for i, line in enumerate(f):
+			if i == 1:
+				w.write("node{} host=".format(i) + line)
+		f.close()
+		w.write("\n[supervisors]\n")
+		f = open(path + '/hosts.txt', 'r')
+		for i, line in enumerate(f):
+			if i >= 2:
+				w.write("node{} host=".format(i) + line)
+		f.close()
+		w.close()
+		os.system('rm -f hosts.txt')
+	
+	
+	@command
+	def do_kubernetes(self, args, arguments):
+		"""
+		::
+	
+          	Usage:
+                	kubernetes name NAME
+			kubernetes size SIZE
+			kubernetes image IMAGE
+			kubernetes flavor FLAVOR
+			kubernetes cloud CLOUD
+			kubernetes cluster info
+			kubernetes cluster deploy
+			kubernetes cluster delete
 
-    def parse_parameters(self, arg):
-        parameters = {}
-        for element in arg:
-            name, value = element.split("=", 1)
-            parameters[name] = value
-        return parameters
+		Arguments:
+		NAME	name of the cluster
+		SIZE	size of the cluster
+		IMAGE	image of the cluster
+		FLAVOR	flavor of the vm
+		CLOUD	cloud on which the cluster will be created
 
-    @command
-    def do_kubernetes(self, args, arguments):
-        """
-        ::
+          	This command does some useful things.
+		"""
+		default = Default()
+		if arguments.name and arguments.NAME:
+			print("Set name to {}.".format(arguments.NAME))
+			default["kubernetes","name"] = arguments.NAME
+		
+		elif arguments.size and arguments.SIZE:
+			print(" Set size to {}.".format(arguments.SIZE))
+			default["kubernetes","size"] = arguments.SIZE
 
-          Usage:
-                kubernetes info
-                kubernetes status
-                kubernetes cloud CLOUD
-                kubernetes image IMAGE
-                kubernetes cluster inventory
-                kubernetes cluster N [CLUSTER]
-                kubernetes deploy [test] 
-                kubernetes benchmark [PARAMETER...]
+		elif arguments.image and arguments.IMAGE:
+			print(" set image to {}.".format(arguments.IMAGE))
+			default["kubernetes","image"] = arguments.IMAGE
+		
+		elif arguments.flavor and arguments.FLAVOR:
+                        print(" set flavor to {}.".format(arguments.FLAVOR))
+                        default["kubernetes","flavor"] = arguments.FLAVOR
 
-          This command does some useful things.
+		elif arguments.cloud and arguments.CLOUD:
+			print(" set cloud to {}.".format(arguments.CLOUD))
+			default["kubernetes","cloud"] = arguments.CLOUD
+		
+		elif arguments.cluster and arguments.info:
+			print("Cluster details:")
+			print("\tCloud\t: {}".format(default["kubernetes","cloud"]))
+			print("\tName\t: {}".format(default["kubernetes","name"]))
+			print("\tSize\t: {}".format(default["kubernetes","size"]))
+			print("\tImage\t: {}".format(default["kubernetes","image"]))
+			print("\tFlavor\t: {}".format(default["kubernetes","flavor"]))
+			print("\nIf any of the above details are None/False,")
+			print("please set them using the appropriate command, before deploying the cluster.")
 
-          Arguments:
-              CLOUD      the name of the cloud
-              IMAGE      the name of the image
-              CLUSTER    the name of the cluster
-              PARAMETER  the paramaters given in form name=value with space separated
+		elif arguments.cluster and arguments.deploy:
+			if default["kubernetes","name"] is not None and default["kubernetes","size"] is not None and default["kubernetes","image"] is not None and default["kubernetes","flavor"] is not None and default["kubernetes","cloud"] is not None:
+				
+				print("Creating cluster {}...".format(default["kubernetes","name"]))
+				# Define a cluster
+				command = "cm cluster define --name {} --count {} --image {} --flavor {} -C {}".format(default["kubernetes","name"], default["kubernetes","size"], default["kubernetes","image"], default["kubernetes","flavor"], default["kubernetes","cloud"])
+				os.system(command)
 
-          Options:
-              -f      specify the file
+				# Use defined cluster
+				command = "cm cluster use {}".format(default["kubernetes","name"])
+				os.system(command)
 
-        """
-        # print(arguments)
+				# Create defined cluster
+				command = "cm cluster allocate"
+				os.system(command)
 
-        default = Default()
+				# Make hosts file
+				self.make_hosts()
 
-        if arguments.info:
+				# Run ansible script
+				#command = 'ansible-playbook -i hosts storm.yml -e "cloud={}"'.format(default["storm","cloud"])
+				#os.system(command)
 
-            Console.error("not yet implemented")
-
-        elif arguments.status:
-
-            # set the status cloud
-            Console.error("not yet implemented")
-
-        elif arguments.cloud:
-
-            default["kubernetes","cloud"] = arguments.CLOUD
-        
-        elif arguments.image:
-
-            default["kubernetes","image"] = arguments.IMAGE
-
-        elif arguments.cluster and arguments.inventory:
-            # export the inventory form the deployed cluster
-            # check this as N could also be inventory
-            Console.error("not yet implemented")
-
-        elif arguments.cluster:
-
-            n = arguments.N
-            name = arguments.CLUSTER
-            if name is None:
-                name = "TBD"
-                #look up the name from the cluster defined
-                pass
-            default["kubernetes","cluster"] = name
-            
-            # set the default cluster with the new name
-            Console.error("not yet implemented")
-
-        elif arguments.deploy and arguments.test:
-
-            # conduct a test on the deployed resources to see if everything is ok
-            Console.error("not yet implemented")
-
-        elif arguments.deploy:
-
-            # conduct the deploy
-            Console.error("not yet implemented")
-
-        elif arguments.benchmark:
-
-            # run the benchmakr with the list of parameters
-            parameters = self.parse_parameters(arguments.PARAMETER)
-
-            pprint (parameters)
-
-            Console.error("not yet implemented")
-
-        default.close()
-        return ""
+				print("Ansible tasks completed.")
+				print("Cluster {} created and storm is running on cluster.".format(default["kubernetes","name"]))
+				default["kubernetes","deploy"] = True
+			else:
+				print("Please set all the required variables.") 
+		default.close()
+		return ""
