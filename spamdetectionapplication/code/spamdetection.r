98aff164359a1559ec78b6788d32c092ddda9794
rahpsing <rahpsing@iu.edu>
2017-07-25 00:38:34 -0400

    new:usr:added algorithm accuracy code and naive bayes spam classifier

--- spamdetectionapplication/code/spamdetection.r
+++ spamdetectionapplication/code/spamdetection.r
@@ -7,8 +7,8 @@ if(!require("e1071"))
   install.packages("e1071",repos = "http://cran.us.r-project.org")
 
 #required package for KNN
-#if(!require("RWeka"))
-#  install.packages("RWeka", dependencies = TRUE,repos = "http://cran.us.r-project.org")
+if(!require("RWeka"))
+  install.packages("RWeka", dependencies = TRUE,repos = "http://cran.us.r-project.org")
 
 #required package for Adaboost
 if(!require("ada"))
@@ -17,254 +17,331 @@ if(!require("ada"))
 if(!require("rbenchmark"))
   install.packages("rbenchmark",repos = "http://cran.us.r-project.org")
 
+if(!require("gclus"))
+  install.packages("gclus",repos = "http://cran.us.r-project.org")
+
+if(!require("MASS"))
+  install.packages("MASS",repos = "http://cran.us.r-project.org")
+
+if(!require("class"))
+  install.packages("class",repos = "http://cran.us.r-project.org")
+
 library(tm)
 library(e1071)
-#library(RWeka)
+library(RWeka)
 library(ada)
 library(rbenchmark)
 
+library(gclus)
+library(MASS)
+library(class)
+
 #Initialize random generator
 set.seed(1245)
-sink("kubernetesoutput.txt")
- ############### Method definitions for the learning process ####################
 
- #This function makes vector (Vector Space Model) from text message using highly repeated words
- getVectorSpaceModel<-function(message,highlyrepeatedwords){
+#sink("output.txt")
 
-	tokenizedmessage<-strsplit(message, "\\s+")[[1]]
+############### Method definitions for the learning process ####################
 
-	#making vector
-	v<-rep(0, length(highlyrepeatedwords))
+#This function makes vector (Vector Space Model) from text message using highly repeated words
+getVectorSpaceModel<-function(message,highlyrepeatedwords){
+  
+  tokenizedmessage<-strsplit(message, "\\s+")[[1]]
+  
+  #making vector
+  v<-rep(0, length(highlyrepeatedwords))
+  
+  for(i in 1:length(highlyrepeatedwords)){
+    for(j in 1:length(tokenizedmessage)){
+      if(highlyrepeatedwords[i]==tokenizedmessage[j]){
+        v[i]<-v[i]+1
+      }
+    }
+  }
+  return (v)
+}
 
-	for(i in 1:length(highlyrepeatedwords)){
-		for(j in 1:length(tokenizedmessage)){
-			if(highlyrepeatedwords[i]==tokenizedmessage[j]){
-				v[i]<-v[i]+1
-			}
-		}
-	}
- return (v)
- }
- 
- svmLinearAnalysis <- function(vtrdata,vtedata) {
-   
-   print("Linear Kernel")
-   svmlinmodel <- svm(x=vtrdata[,2:length(vtrdata[1,])],y=vtrdata[,1],type='C', kernel='linear');
-   summary(svmlinmodel)
-   predictionlin <- predict(svmlinmodel, vtedata[,2:length(vtedata[1,])])
-   tablinear <- table(pred = predictionlin , true = vtedata[,1]); tablinear
-   precisionlin<-sum(diag(tablinear))/sum(tablinear);
-   print("General Error using Linear SVM is (in percent):");(1-precisionlin)*100
-   print("Ham Error using Linear SVM is (in percent):");(tablinear[1,2]/sum(tablinear[,2]))*100
-   print("Spam Error using Linear SVM is (in percent):");(tablinear[2,1]/sum(tablinear[,1]))*100
- }
- 
- 
- svmPolynomialAnalysis <- function(vtrdata,vtedata) {
-   
-   svmpolymodel <- svm(x=vtrdata[,2:length(vtrdata[1,])],y=vtrdata[,1], kernel='polynomial', probability=FALSE)
-   summary(svmpolymodel)
-   predictionpoly <- predict(svmpolymodel, vtedata[,2:length(vtedata[1,])])
-   tabpoly <- table(pred = predictionpoly , true = vtedata[,1]); tabpoly
- }
- 
- svmRadialAnalysis <- function(vtrdata,vtedata) {
-   
-   svmradmodel <- svm(x=vtrdata[,2:length(vtrdata[1,])],y=vtrdata[,1], kernel = "radial", gamma = 0.09, cost = 1, probability=FALSE)
-   summary(svmradmodel)
-   predictionrad <- predict(svmradmodel, vtedata[,2:length(vtedata[1,])])
-   tabrad <- table(pred = predictionrad, true = vtedata[,1]); tabrad
- }
+svmLinearAnalysis <- function(vtrdata,vtedata) {
+  
+  print("Linear Kernel")
+  svmlinmodel <- svm(x=vtrdata[,2:length(vtrdata[1,])],y=vtrdata[,1],type='C', kernel='linear');
+  summary(svmlinmodel)
+  predictionlin <- predict(svmlinmodel, vtedata[,2:length(vtedata[1,])])
+  tablinear <- table(pred = predictionlin , true = vtedata[,1]); tablinear
+  precisionlin<-sum(diag(tablinear))/sum(tablinear);
 
- kNearestNeighbourAnalysis <- function(vtrdata,vtedata) {
-   
-   data<-data.frame(sms=vtrdata[,2:length(vtrdata[1,])],type=vtrdata[,1])
-   classifier <- IBk(data, control = Weka_control(K = 20, X = TRUE))
-   summary(classifier)
-   evaluate_Weka_classifier(classifier, newdata = data.frame(sms=vtedata[,2:length(vtedata[1,])],type=vtedata[,1]))
- }
- 
- adaBoostAnalysis <-  function(vtrdata,vtedata) {
-   
-   adaptiveboost<-ada(x=vtrdata[,2:length(vtrdata[1,])],y=vtrdata[,1],test.x=vtedata[,2:length(vtedata[1,])], test.y=vtedata[,1], loss="logistic", type="gentle", iter=100)
-   summary(adaptiveboost)
-   varplot(adaptiveboost)
- }
- 
- vectorizeTrainingAndTestData <- function(data, highlyrepeatedwords) {
-   
-   vectoredData = NULL;
-   
-   for(i in 1:length(data[,2])){
-     if(data[i,1]=="ham"){
-       vectoredData=rbind(vectoredData,c(1,getVectorSpaceModel(data[i,2],highlyrepeatedwords)))
-     }
-     else{
-       vectoredData=rbind(vectoredData,c(0,getVectorSpaceModel(data[i,2],highlyrepeatedwords)))
-     }
-   }
-   
-   return (vectoredData)
- }
+  cat(sprintf("General Error using Linear SVM is (in percent): %f",((1-precisionlin)*100)))
 
- ############### End - Method definitions for the learning process ####################
+  cat(sprintf("Ham Error using Linear SVM is (in percent): %f",((tablinear[1,2]/sum(tablinear[,2]))*100)))
+
+  cat(sprintf("Spam Error using Linear SVM is (in percent): %f",((tablinear[2,1]/sum(tablinear[,1]))*100)))
+  
+  accuracy = 0
+  for (i in 1 : nrow(tablinear)) {
+    for (j in 1 : ncol(tablinear)) {
+      if (i == j) {
+        accuracy = accuracy + tablinear[i, i]
+      }
+    }
+  }
+  accuracy = (accuracy / sum(tablinear)) * 100
+  cat("Test vectors:", dim(vtedata), ", Accuracy =", accuracy, "%\n")
+}
+
+convert_count <- function(x) {
+  y <- ifelse(x > 0, 1,0)
+  y <- factor(y, levels=c(0,1), labels=c("No", "Yes"))
+  y
+}
+
+svmPolynomialAnalysis <- function(vtrdata,vtedata) {
+  
+  svmpolymodel <- svm(x=vtrdata[,2:length(vtrdata[1,])],y=vtrdata[,1], kernel='polynomial', probability=FALSE)
+  print("summary:")
+  summary(svmpolymodel)
+
+  print("prediction poly data:")
+ # print(predictionpoly)
+  predictionpoly <- predict(svmpolymodel, vtedata[,2:length(vtedata[1,])])
+  for(i in 1:length(predictionpoly)) {
+    
+    if(predictionpoly[i] > 0.6)
+      predictionpoly[i] = 1
+    else
+      predictionpoly[i] = 0
+  }
+  print("tabpoly data:")
+  tabpoly <- table(pred = predictionpoly , true = vtedata[,1]);tabpoly
+  print(tabpoly)
+  
+  accuracy = 0
+  for (i in 1 : nrow(tabpoly)) {
+    for (j in 1 : ncol(tabpoly)) {
+      if (i == j) {
+        accuracy = accuracy + tabpoly[i, i]
+      }
+    }
+  }
+  accuracy = (accuracy / sum(tabpoly)) * 100
+  cat("Test vectors:", dim(vtedata), ", Accuracy =", accuracy, "%\n")
+  
+}
+
+svmRadialAnalysis <- function(vtrdata,vtedata) {
+  
+  svmradmodel <- svm(x=vtrdata[,2:length(vtrdata[1,])],y=vtrdata[,1], kernel = "radial", gamma = 0.09, cost = 1, probability=FALSE)
+  summary(svmradmodel)
+  predictionrad <- predict(svmradmodel, vtedata[,2:length(vtedata[1,])])
+  
+  for(i in 1:length(predictionrad)) {
+    
+    if(predictionrad[i] > 0.6)
+      predictionrad[i] = 1
+    else
+      predictionrad[i] = 0
+  }
+  
+  tabrad <- table(pred = predictionrad, true = vtedata[,1]); tabrad
+  
+  accuracy = 0
+  for (i in 1 : nrow(tabrad)) {
+    for (j in 1 : ncol(tabrad)) {
+      if (i == j) {
+        accuracy = accuracy + tabrad[i, i]
+      }
+    }
+  }
+  accuracy = (accuracy / sum(tabrad)) * 100
+  cat("Test vectors:", dim(vtedata), ", Accuracy =", accuracy, "%\n")
+}
+
+kNearestNeighbourAnalysis <- function(vtrdata,vtedata) {
+  
+  data<-data.frame(sms=vtrdata[,2:length(vtrdata[1,])],type=vtrdata[,1])
+  classifier <- IBk(data, control = Weka_control(K = 20, X = TRUE))
+  summary(classifier)
+  evaluate_Weka_classifier(classifier, newdata = data.frame(sms=vtedata[,2:length(vtedata[1,])],type=vtedata[,1]))
+  predict_res <- predict(classifier, newdata = data.frame(sms=vtedata[,2:length(vtedata[1,])], type = vtedata[,1]), type = ) 
+
+}
+
+naiveBayesAnalysis <-  function(vtrdata,vtedata) {
+#---------------------Naive Bayes----------------------------#
+
+sms_train <- apply(vtrdata[,2:length(vtrdata[1,])], 2, convert_count)
+sms_test <- apply(vtedata[,2:length(vtedata[1,])], 2, convert_count)
+sms_classifier <- naiveBayes(sms_train, factor(vtrdata[,1]))
+class(sms_classifier)
+sms_test_pred <- predict(sms_classifier, newdata=sms_test)
+
+tab_bayes <- table(sms_test_pred, vtedata[,1])
+
+accuracy = 0
+for (i in 1 : nrow(tab_bayes)) {
+  for (j in 1 : ncol(tab_bayes)) {
+    if (i == j) {
+      accuracy = accuracy + tab_bayes[i, i]
+    }
+  }
+}
+accuracy = (accuracy / sum(tab_bayes)) * 100
+cat("Test vectors:", dim(vtedata), ", Accuracy =", accuracy, "%\n")
+}
+
+adaBoostAnalysis <-  function(vtrdata,vtedata) {
+  
+  adaptiveboost<-ada(x=vtrdata[,2:length(vtrdata[1,])],y=vtrdata[,1],test.x=vtedata[,2:length(vtedata[1,])], test.y=vtedata[,1], loss="logistic", type="gentle", iter=100)
+  summary(adaptiveboost)
+  varplot(adaptiveboost)
+}
+
+vectorizeTrainingAndTestData <- function(data, highlyrepeatedwords) {
+  
+  vectoredData = NULL;
+  
+  for(i in 1:length(data[,2])){
+    if(data[i,1]=="ham"){
+      vectoredData=rbind(vectoredData,c(1,getVectorSpaceModel(data[i,2],highlyrepeatedwords)))
+    }
+    else{
+      vectoredData=rbind(vectoredData,c(0,getVectorSpaceModel(data[i,2],highlyrepeatedwords)))
+    }
+  }
+  
+  return (vectoredData)
+}
+
+############### End - Method definitions for the learning process ####################
 ca = c('SMSSpamCollection.txt','SMSSpamCollection1.txt','SMSSpamCollection2.txt')
 cb = c("benchmark_comparisions.png","benchmark_comparisions1.png","benchmark_comparisions2.png")
-for (kk in 1:3) #Unsafe editing
-{  		#Unsafe editing
-#loading data. Original data is from http://archive.ics.uci.edu/ml/datasets/SMS+Spam+Collection
- print("Uploading SMS Spam dataset")
- smstable<-read.csv(ca[kk], header = FALSE, quote = "", sep = "\t", colClasses=c('character','character'))
- 
- 
- n = nrow(smstable);
- mysample = sample(1:nrow(smstable),n/2)  # dividing the training and testing set
- 
- training_data = smstable[mysample,]
- testing_data = smstable[-mysample,]
- 
-#smstabletmp<-smstable
-smstabletmp<-training_data
+#for (kk in 1:3) #Unsafe editing
+#{  		#Unsafe editing
+  #loading data. Original data is from http://archive.ics.uci.edu/ml/datasets/SMS+Spam+Collection
+  print("Uploading SMS Spam dataset")
+  smstable<-read.csv(ca[1], header = FALSE, quote = "", sep = "\t", colClasses=c('character','character'))
+  
+  
+  n = nrow(smstable);
+  mysample = sample(1:nrow(smstable),n/2)  # dividing the training and testing set
+  
+  training_data = smstable[mysample,]
+  testing_data = smstable[-mysample,]
+  
+  #smstabletmp<-smstable
+  smstabletmp<-training_data
+  
+  print("Extracting Ham and Spam Basic Statistics!")
+  
+  smstabletmp$type[smstabletmp$V1=="ham"] <- 1
+  smstabletmp$type[smstabletmp$V1=="spam"] <- 0
+  
+  #Convert character data ismstanto numeric
+  tmp<-as.numeric(smstabletmp$type)
+  
+  #Basic Statisctics like mean and variance of spam and hams
+  hamavg<-mean(tmp)
+  print("Average Ham is :");hamavg
+  
+  hamvariance<-var(tmp)
+  print("Var of Ham is :");hamvariance
+  
+  print("Extract average token of Hams and Spams!")
+  
+  nohamtokens<-0
+  noham<-0
+  nospamtokens<-0
+  nospam<-0
+  
+  for(i in 1:length(smstabletmp$type)){
+    if(smstabletmp[i,1]=="ham"){
+      nohamtokens<-length(strsplit(smstabletmp[i,2], "\\s+")[[1]])+nohamtokens
+      noham<-noham+1
+    }else{ 
+      nospamtokens<-length(strsplit(smstabletmp[i,2], "\\s+")[[1]])+nospamtokens
+      nospam<-nospam+1
+    }
+  }
+  
+  totaltokens<-nospamtokens+nohamtokens;
+  print("total number of tokens is:")
+  print(totaltokens)
+  
+  avgtokenperham<-nohamtokens/noham
+  print("Avarage number of tokens per ham message")
+  print(avgtokenperham)
+  
+  avgtokenperspam<-nospamtokens/nospam
+  print("Avarage number of tokens per spam message")
+  print(avgtokenperspam)
+  
+  print(" Make two different sets, training data and test data!")
+  
+  #select the percent of data that you want to use as training set
+  trdatapercent<-0.3
+  
+  #training data set
+  
+  trdata=NULL
+  
+  #test data set
+  tedata=NULL
+  
+  for(i in 1:length(smstabletmp$type)){
+    if(runif(1)<trdatapercent){
+      trdata=rbind(trdata,c(smstabletmp[i,1],tolower(smstabletmp[i,2])))
+    }
+    else {
+      tedata=rbind(tedata,c(smstabletmp[i,1],tolower(smstabletmp[i,2])))
+    }
+  }
+  
+  print("Training data size is!")
+  dim(trdata)
+  
+  print("Test data size is!")
+  dim(tedata)
+  
+  # Text feature extraction using tm package
+  
+  trsmses<-Corpus(VectorSource(trdata[,2]))
+  trsmses<-tm_map(trsmses, stripWhitespace)
+  trsmses<-tm_map(trsmses, tolower)
+  trsmses<-tm_map(trsmses, removeWords, stopwords("english"))
+  
+  dtm <- DocumentTermMatrix(trsmses)
+  
+  highlyrepeatedwords<-findFreqTerms(dtm, 80)
+  
+  #These highly used words are used as an index to make VSM 
+  #(vector space model) for trained data and test data
+  
+  #vectorized training data set
+  vtrdata = vectorizeTrainingAndTestData(trdata, highlyrepeatedwords);
+  
+  #vectorized test data set 
+  vtedata = vectorizeTrainingAndTestData(tedata, highlyrepeatedwords);
+  
+  #Naive Bayes to be added
+  
+  
+  # Run different classification algorithms
+  # differnet SVMs with different Kernels
+  print("----------------------------------SVM-----------------------------------------")
+  svmLinearAnalysis(vtrdata,vtedata);
+  
+  print("Polynomial Kernel")
+  svmPolynomialAnalysis(vtrdata,vtedata);
+  
+  print("Radial Kernel")
+  svmRadialAnalysis(vtrdata,vtedata);
+  
+  #print("----------------------------------AdaBoost-----------------------------------")
+  adaBoostAnalysis(vtrdata,vtedata);
+  
+  #print("----------------------------------NaiveBayes-----------------------------------")
+  naiveBayesAnalysis(vtrdata,vtedata);
+  
+  #print("----------------------------------KNN-----------------------------------------")
+  kNearestNeighbourAnalysis(vtrdata,vtedata)
+  
 
-print("Extracting Ham and Spam Basic Statistics!")
-
-smstabletmp$type[smstabletmp$V1=="ham"] <- 1
-smstabletmp$type[smstabletmp$V1=="spam"] <- 0
-
-#Convert character data ismstanto numeric
-tmp<-as.numeric(smstabletmp$type)
-
-#Basic Statisctics like mean and variance of spam and hams
-hamavg<-mean(tmp)
-print("Average Ham is :");hamavg
-
-hamvariance<-var(tmp)
-print("Var of Ham is :");hamvariance
-
-print("Extract average token of Hams and Spams!")
-
-nohamtokens<-0
-noham<-0
-nospamtokens<-0
-nospam<-0
-
-for(i in 1:length(smstabletmp$type)){
-	if(smstabletmp[i,1]=="ham"){
-		nohamtokens<-length(strsplit(smstabletmp[i,2], "\\s+")[[1]])+nohamtokens
-		noham<-noham+1
-	}else{ 
-		nospamtokens<-length(strsplit(smstabletmp[i,2], "\\s+")[[1]])+nospamtokens
-		nospam<-nospam+1
-	}
-}
-
-totaltokens<-nospamtokens+nohamtokens;
-print("total number of tokens is:")
-print(totaltokens)
-
-avgtokenperham<-nohamtokens/noham
-print("Avarage number of tokens per ham message")
-print(avgtokenperham)
-
-avgtokenperspam<-nospamtokens/nospam
-print("Avarage number of tokens per spam message")
-print(avgtokenperspam)
-
-print(" Make two different sets, training data and test data!")
-
-#select the percent of data that you want to use as training set
-trdatapercent<-0.3
-
-#training data set
-
-trdata=NULL
-
-#test data set
-tedata=NULL
-
-for(i in 1:length(smstabletmp$type)){
-	if(runif(1)<trdatapercent){
-		trdata=rbind(trdata,c(smstabletmp[i,1],tolower(smstabletmp[i,2])))
-	}
-	else {
-	tedata=rbind(tedata,c(smstabletmp[i,1],tolower(smstabletmp[i,2])))
-	}
-}
-
-print("Training data size is!")
-dim(trdata)
-
-print("Test data size is!")
-dim(tedata)
-
-# Text feature extraction using tm package
-
-trsmses<-Corpus(VectorSource(trdata[,2]))
-trsmses<-tm_map(trsmses, stripWhitespace)
-trsmses<-tm_map(trsmses, tolower)
-trsmses<-tm_map(trsmses, removeWords, stopwords("english"))
-
-dtm <- DocumentTermMatrix(trsmses)
-
-highlyrepeatedwords<-findFreqTerms(dtm, 80)
-
-#These highly used words are used as an index to make VSM 
-#(vector space model) for trained data and test data
-
-#vectorized training data set
-vtrdata = vectorizeTrainingAndTestData(trdata, highlyrepeatedwords);
-
-#vectorized test data set 
-vtedata = vectorizeTrainingAndTestData(tedata, highlyrepeatedwords);
-
-#Naive Bayes to be added
-
-
-# Run different classification algorithms
-# differnet SVMs with different Kernels
-print("----------------------------------SVM-----------------------------------------")
-svmLinearAnalysis(vtrdata,vtedata);
-
-print("Polynomial Kernel")
-svmPolynomialAnalysis(vtrdata,vtedata);
-
-print("Radial Kernel")
-svmRadialAnalysis(vtrdata,vtedata);
-
-#print("----------------------------------KNN-----------------------------------------")
-#kNearestNeighbourAnalysis(vtrdata,vtedata)
-
-# benchmark different classifiers for given algorithms
-benchmark_data1 = benchmark( replications = 10, svmLinearAnalysis(vtrdata, vtedata), svmPolynomialAnalysis(vtrdata, vtedata),
-                            svmRadialAnalysis(vtrdata, vtedata),
-                            order = NULL)
-
-benchmark_data2 = benchmark( replications = 10, svmLinearAnalysis(vtrdata, vtedata), svmPolynomialAnalysis(vtrdata, vtedata),
-                            svmRadialAnalysis(vtrdata, vtedata),
-                            order = NULL)
-
-benchmark_data3 = benchmark( replications = 10, svmLinearAnalysis(vtrdata, vtedata), svmPolynomialAnalysis(vtrdata, vtedata),
-                            svmRadialAnalysis(vtrdata, vtedata),
-                            order = NULL)
-
-#code to plot the benchmarks in a jpg file
-nreplications = c(10,10,10)
-
-                      
-svmLinear = c(benchmark_data1$elapsed[1], benchmark_data2$elapsed[1], benchmark_data3$elapsed[1])
-svmPolynomial = c(benchmark_data1$elapsed[2], benchmark_data2$elapsed[2], benchmark_data3$elapsed[2])
-svmRadial = c(benchmark_data1$elapsed[3], benchmark_data2$elapsed[3], benchmark_data3$elapsed[3])
-
-# Give the chart file a name.
-png(file = cb[kk])
-
-# Plot the bar chart.
-plot(x = nreplications,y = svmLinear,type = "o",col = "red", xlab = "Replications", ylab = "Time Elapsed", 
-     main = "Replications vs Time")
-
-lines(x = nreplications, y = svmPolynomial, type = "o", col = "blue")
-lines(x = nreplications, y = svmRadial, type = "o", col = "green")
-#print(i)
-# Save the file.
-dev.off()
-} #Unsafe editing
